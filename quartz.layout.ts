d64114886b94a54df33c1d78a049e60933cc7fc9
ShapeNaN <str.doublet@gmail.com>
2025-05-18 13:33:07 +0300

    Quartz sync: May 18, 2025, 1:33 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,7 +48,14 @@ export const defaultContentPageLayout: PageLayout = {
         repelForce: 0.6, // how much nodes should repel each other
         centerForce: 0.2, // how much force to use when trying to center the nodes
         linkDistance: 40, // how long should the links be by default?
-        fontSize: 0.4, // what size should the node labels be?
+        fontSize: 0.6, // what size should the node labels be?
+      },
+      globalGraph: {
+        enableRadial: true,
+        repelForce: 0.6, // how much nodes should repel each other
+        centerForce: 0.2, // how much force to use when trying to center the nodes
+        linkDistance: 40, // how long should the links be by default?
+        fontSize: 0.4,
       },
     }),
     Component.DesktopOnly(Component.TableOfContents()),
